3c62bc27dc7141283d79a7ec97ce27f6375bcc46
Bader Latarche <122647283+baderlt@users.noreply.github.com>
2026-05-06 00:44:23 +0100

    Fix dynamic color application and expand FR/AR translations

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class SetLocale
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $locale = session('locale', config('app.locale'));
+        if (! in_array($locale, ['fr', 'ar'], true)) {
+            $locale = 'fr';
+        }
+
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
