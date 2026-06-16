fa98cbb4cf90324116a02231a2a8cdf53d90af75
Bader Latarche <122647283+baderlt@users.noreply.github.com>
2026-06-16 14:05:38 +0100

    Fix responsive product card sizing

--- resources/views/products/_cards.blade.php
+++ resources/views/products/_cards.blade.php
@@ -1,5 +1,5 @@
 @foreach($products as $product)
-    <div class="product-grid-item flex h-full">
+    <div class="product-grid-item flex h-full min-w-0">
         @include('products._card', ['product' => $product])
     </div>
 @endforeach
