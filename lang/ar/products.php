d5bf7c89b1ef8e2ac0c8fef406ad09dd5178f387
Bader Latarche <122647283+baderlt@users.noreply.github.com>
2026-06-16 15:16:53 +0100

    Shorten French quantity CTA

--- lang/ar/products.php
+++ lang/ar/products.php
@@ -1,2 +1,2 @@
 <?php
-return ['none_found' => 'لم يتم العثور على منتجات', 'sku' => 'رمز المنتج', 'add_to_pack' => 'أضف إلى الباقة', 'category' => 'الفئة'];
+return ['none_found' => 'لم يتم العثور على منتجات', 'sku' => 'رمز المنتج', 'add_to_pack' => 'أضف إلى الباقة', 'choose_quantity' => 'اختر الكمية', 'category' => 'الفئة'];
